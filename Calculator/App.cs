21e946dc32a38dd47d71bd6e231c91eb2012af10
hogst <hogst@STATIONR>
2024-11-05 21:25:59 +0100

    Add project files.

---
+++ Calculator/App.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class App
+    {
+        public void Start()
+        {
+            var calculator = new Calculator<int>();
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Make your calculation below: ");
+                    Console.WriteLine(calculator.ParseAndCalculate(Console.ReadLine()));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,69 @@
+﻿namespace Calculator
+{
+    public enum Operation
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+        Rest
+    }
+
+    public class Calculator<T>
+    {
+        public T Calculate(T numberOne, T numberTwo, Operation operation)
+        {
+            while (true)
+            {
+                switch (operation)
+                {
+                    case Operation.Add:
+                        return (dynamic)numberOne + (dynamic)numberTwo;
+                        break;
+                    case Operation.Subtract:
+                        return (dynamic)numberOne - (dynamic)numberTwo;
+                        break;
+                    case Operation.Multiply:
+                        return (dynamic)numberOne * (dynamic)numberTwo;
+                        break;
+                    case Operation.Divide:
+                        if ((dynamic)numberTwo == 0)
+                            Console.WriteLine("Du kan inte dividera ett tal med 0.");
+                        return (dynamic)numberOne / (dynamic)numberTwo;
+                        break;
+                    case Operation.Rest:
+                        return (dynamic)numberOne % (dynamic)numberTwo;
+                        break;
+                    default:
+                        Console.WriteLine("Try with a correct operator: +, -, *, / or %");
+                        break;
+                }
+            }
+        }
+
+        public T ParseAndCalculate(string input)
+        {
+            var parts = input.Split(' ');
+            if (parts.Length != 3)
+            {
+                Console.WriteLine($"Utärkningen behöver anges enligt formatet: '1 + 2'");
+            }
+
+            T numberOne = (T)Convert.ChangeType(parts[0], typeof(T));
+            T numberTwo = (T)Convert.ChangeType(parts[2], typeof(T));
+
+            Operation operation = parts[1] switch
+            {
+                "+" => Operation.Add,
+                "-" => Operation.Subtract,
+                "*" => Operation.Multiply,
+                "/" => Operation.Divide,
+                "%" => Operation.Rest,
+                _ => throw new InvalidOperationException($"Ogiltig operator. Använd +, -, *, / eller %")
+            };
+
+            return Calculate(numberOne, numberTwo, operation);
+        }
+    }
+}
+
